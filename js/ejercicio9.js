5bbec3418f32468846be3e10314c67465be1ad7f
Agostina <agostinamaranzana@gmail.com>
2021-10-03 20:20:07 -0300

    agregado el ejercicio 9 y listo

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,1 +1,13 @@
-let frase = prompt('Ingrese una frase')
+let frase = prompt('Ingrese una frase');
+
+console.log(frase);
+console.log(frase.length)
+console.log(frase.charAt(0))
+
+// toUppercase toLowercase    para pasar todo a mayuscula o min
+
+for(let posicion = 0; posicion < frase.length; posicion++){
+    if(frase.charAt(posicion) == 'a' ||frase.charAt(posicion) == 'e' ||frase.charAt(posicion) == 'i' ||frase.charAt(posicion) == 'o' ||frase.charAt(posicion) == 'u'){
+        document.write(frase.charAt(posicion));
+    }
+}
